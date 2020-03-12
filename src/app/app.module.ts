907cd664c315fb61556ce7bfad20deacef5a1755
Nacer Bouchekhima <nbouchek@gmail.com>
2020-03-12 00:08:38 +0000

    add imports for ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ProductListComponent } from "./components/product-list/product-list.com
 import { HttpClientModule } from "@angular/common/http";
 import { ProductService } from "./services/product.service";
 import { Routes, RouterModule } from "@angular/router";
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductCategoryMenuComponent } from "./components/product-category-menu/product-category-menu.component";
+import { SearchComponent } from "./components/search/search.component";
+import { ProductDetailsComponent } from "./components/product-details/product-details.component";
+
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 const routes: Routes = [
-  { path: "products/:id", component: ProductDetailsComponent},
-  { path: "search/:keyword", component: ProductListComponent},
+  { path: "products/:id", component: ProductDetailsComponent },
+  { path: "search/:keyword", component: ProductListComponent },
   { path: "category/:id", component: ProductListComponent },
   { path: "category", component: ProductListComponent },
   { path: "products", component: ProductListComponent },
@@ -21,8 +23,19 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent, SearchComponent, ProductDetailsComponent],
-  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule,
+    NgbModule
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
